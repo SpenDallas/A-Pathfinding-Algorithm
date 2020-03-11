7664fd698206cdd053917b52ebdbdb44fab4e3c3
Daniel Oh <16dhjo@caslab.queensu.ca>
2020-03-11 19:55:55 -0400

    implemented file IO

--- DanFile.py
+++ DanFile.py
@@ -4,9 +4,24 @@ dan file bro
 
 
 def main():
+    # file input
     with open("pathfinding_a.txt", 'r') as f_input:
-        file_list = [int(x) for x in f_input.readlines()]
+        grid = [[x for x in line if x != "\n"] for line in f_input.readlines()]
 
+    #
+    # the grid variable is the initial state; do stuff and set the solution to grid
+    #
+
+    # file output
+    with open("pathfinding_a_out.txt", 'w') as f_output:
+        output_list = []
+        # convert to a 1D list of strings
+        for line in grid:
+            output_list.append(''.join(line) + "\n")
+        # removing last "\n"
+        output_list[-1] = output_list[-1][:-1]
+        # writing to the file
+        f_output.writelines(output_list)
 
 if __name__ == "__main__":
     main()

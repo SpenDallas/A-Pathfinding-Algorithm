4e06e8b86398014e5af091b3ff1a47377e088b6f
Daniel Oh <16dhjo@caslab.queensu.ca>
2020-03-11 19:08:51 -0400

    added files we'll need

---
+++ DanFile.py
@@ -0,0 +1,12 @@
+'''
+dan file bro
+'''
+
+
+def main():
+    with open("pathfinding_a.txt", 'r') as f_input:
+        file_list = [int(x) for x in f_input.readlines()]
+
+
+if __name__ == "__main__":
+    main()
